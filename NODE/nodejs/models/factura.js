c4d463871b556ecc84ed83b5c364ecb8595dc023
EduardoRMgit <122333594+EduardoRMgit@users.noreply.github.com>
2023-08-18 20:19:58 -0600

    Modelo factura y Mesero
    Se agregó Factura y mesero

---
+++ NODE/nodejs/models/factura.js
@@ -0,0 +1,55 @@
+'use strict';
+const {
+  Model,
+  DataTypes
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Factura extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+
+  Factura.init({
+    idFactura: {type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false,},
+
+    fecha: DataTypes.DATEONLY,
+
+    idMesero: {type: DataTypes.INTEGER,
+               allowNull: false,},
+
+    mesa: {type: DataTypes.INTEGER,
+            allowNull: false,},
+        
+    subTotal: {type: DataTypes.FLOAT,
+               allowNull: false,
+               set(value) {
+               if (!isNaN(parseFloat(value))) {
+                    this.setDataValue('subTotal', parseFloat(value.toFixed(2)));
+               }
+               },
+               },
+            
+    total: {type: DataTypes.FLOAT,
+               allowNull: false,
+               set(value) {
+               if (!isNaN(parseFloat(value))) {
+                    this.setDataValue('total', parseFloat(value.toFixed(2)));
+               }
+               },
+               },
+  }, {
+    sequelize,
+    modelName: 'Factura',
+  });
+  return Factura;
+};
